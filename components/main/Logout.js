f48220a0e9469c939a8e31e14041f56868919c67
atif9281 <atiff.khan92810000@gmail.com>
2024-07-09 13:06:30 +0500

    logout change

--- components/main/Logout.js
+++ components/main/Logout.js
@@ -6,8 +6,8 @@ const Logout = ({clickingLogout}) => {
 
 
   return (
-    <div className='m-9'>
-        <button className='bg-slate-500 text-white hover:bg-slate-600  mx-2 rounded p-1' onClick={clickingLogout}>Log out</button>
+    <div className='m-9 flex justify-center'>
+        <button className='bg-slate-500 text-white hover:bg-slate-600  mx-2 rounded p-1 ' onClick={clickingLogout}>Log out</button>
     </div>
   );
 };
